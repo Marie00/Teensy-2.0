02027e774b7f3791fce625b728c9bf645fb098ad
Marie00 <ramzi90000@gmail.com>
2015-12-10 12:31:27 +0100

    final

--- Achievement_login_mac/Achievement_login_mac.ino
+++ Achievement_login_mac/Achievement_login_mac.ino
@@ -3,32 +3,37 @@ void setup() {
   //Mouse.begin();
 }
 
-void loop() {
-
-  int i = 0;
-
+void  logIn()
+{
   Keyboard.write("rabougue\n");
-  Keyboard.write("/*Mot de passe ici*/");
+  Keyboard.write("/*Mot de passe*/\n");
   delay(1000);
   Keyboard.write("\n");
-  delay(2000);
+  delay(1000);
   Keyboard.write("\n");
   delay(10000);
-  Mouse.move(5000, 5000, 0);// Curseur se deplace de 5000 pixels en haut et à droite
-  Mouse.move(5000, 5000, 0);// Curseur se deplace de 5000 pixels en haut et à droite
-  Mouse.move(5000, 5000, 0);// Curseur se deplace de 5000 pixels en haut et à droite
+}
+
+void  logOut(void)
+{
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('q');
   delay(1000);
-  Mouse.move(20, 0, 0);// Curseur se deplace de 20 pixels à gauche
-  delay(1000);
-  Mouse.click(MOUSE_LEFT);
+  Keyboard.releaseAll();
+  Keyboard.write("\n"); //Simule entrée pour confirmation de message de deconexion
   delay(500);
-  Mouse.move(0, -2500, 0);
+  Keyboard.write("\n");//Simule entrée pour confirmation de message de deconexion
   delay(500);
-  Mouse.click(MOUSE_LEFT);
+  Keyboard.write("\n");//Simule entrée pour confirmation de message de deconexion
+  delay(500);
+  Keyboard.write("\n");//Simule entrée pour confirmation de message de deconexion
   delay(5000);
-  if (i > 0)
-  {
-    Keyboard.write("\n");
-  }
-  i++;
+}
+
+void loop() 
+{
+  delay(1000);
+  logIn();
+  logOut();
 }
